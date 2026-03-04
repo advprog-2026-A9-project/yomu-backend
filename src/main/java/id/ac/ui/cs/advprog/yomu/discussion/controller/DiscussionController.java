a612a8e70134ce8b962cfcf2db8c8a3122c523bd
Acid <Acid-aja@users.noreply.github.com>
2026-03-04 21:14:06 +0700

    feat(discussion): add Discussion Controller

---
+++ src/main/java/id/ac/ui/cs/advprog/yomu/discussion/controller/DiscussionController.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.yomu.discussion.controller;
+
+import id.ac.ui.cs.advprog.yomu.discussion.dto.CommentResponse;
+import id.ac.ui.cs.advprog.yomu.discussion.dto.CreateCommentRequest;
+import id.ac.ui.cs.advprog.yomu.discussion.service.DiscussionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/discussion")
+@RequiredArgsConstructor
+public class DiscussionController {
+
+    private final DiscussionService discussionService;
+
+    @PostMapping("/create")
+    public ResponseEntity<CommentResponse> createComment(@RequestBody CreateCommentRequest request) {
+        return ResponseEntity.ok(discussionService.createComment(request));
+    }
+
+    @GetMapping("/reading/{readingId}")
+    public ResponseEntity<List<CommentResponse>> getCommentsByReading(@PathVariable UUID readingId) {
+        return ResponseEntity.ok(discussionService.getCommentsByReading(readingId));
+    }
+}
