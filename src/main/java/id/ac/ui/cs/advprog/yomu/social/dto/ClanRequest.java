99afc4fb89ebc22f94bb189e6a74507f62e1f1fc
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-03-06 13:20:40 +0700

    [GREEN] feat: implement clan models and request DTOs

---
+++ src/main/java/id/ac/ui/cs/advprog/yomu/social/dto/ClanRequest.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.yomu.social.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ClanRequest {
+    private String name;
+    private String description;
+    private String userId;
+}
