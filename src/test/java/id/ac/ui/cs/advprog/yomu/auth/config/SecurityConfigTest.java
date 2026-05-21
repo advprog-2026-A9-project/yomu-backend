1adffe2ef954cc2293a43cde0f5475a515f43b71
ForgerPhysics <sihalohoryan2@gmail.com>
2026-05-22 00:16:45 +0700

    fix dependency injection unit test

--- src/test/java/id/ac/ui/cs/advprog/yomu/auth/config/SecurityConfigTest.java
+++ src/test/java/id/ac/ui/cs/advprog/yomu/auth/config/SecurityConfigTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class SecurityConfigTest {
 
     private static final String RESPONSE_NOT_NULL = "Response should not be null";
