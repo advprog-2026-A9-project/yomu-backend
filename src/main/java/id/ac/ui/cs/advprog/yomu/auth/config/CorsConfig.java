1c475db161ad4b27f76cdcb080cc999280e1a3d1
piesfication <muhammad.rafi42@ui.ac.id>
2026-05-22 02:01:38 +0700

    [FIX] Update CORS whitelist for staging domain

--- src/main/java/id/ac/ui/cs/advprog/yomu/auth/config/CorsConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/yomu/auth/config/CorsConfig.java
@@ -17,6 +17,7 @@ public class CorsConfig {
         config.setAllowedOriginPatterns(List.of(
             "http://localhost:*",
             "http://127.0.0.1:*",
+            "https://staging.yomu.info",
             "https://*.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
